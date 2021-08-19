fe0f68591ae13bd508173460f18d4eaf539189ac
vuduynhiennn <vuduynhiencute@gmail.com>
2021-08-20 03:18:06 +0700

    nodejs

---
+++ chương trình đầu tiên/app.js
@@ -0,0 +1,10 @@
+var person = {
+    firstName : "Nhien",
+    lastName : "Vu",
+    sayHello: function() {
+        console.log("say hello" + " " + this.lastName + " " + this.firstName);
+    }
+}
+person.sayHello();
+console.log(person["firstName"];
+console.log(person["firstName"];
---
+++ chương trình đầu tiên/hello.js
@@ -0,0 +1,7 @@
+
+
+var sayHello = function() {
+    console.log("hello this is my first time learning nodejs");
+}
+
+module.exports = sayHello;
